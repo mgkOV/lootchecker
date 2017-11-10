f1e76493fed5e9d1ecf534aa19d4238260b5d6a7
mgkOV <myagkov.mww@gmail.com>
2017-11-10 17:19:28 +0300

    persist balance in cookie

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -1,16 +1,28 @@
 import c from '../actions/constants';
+import { read_cookie, bake_cookie } from 'sfcookies';
+
+const BALANCE_COOKIE = 'BALANCE_COOKIE';
 
 const balanceReducer = (state = 0, action) => {
+  let balance;
+
   switch (action.type) {
     case c.SET_BALANCE:
-      return action.payload;
+      balance = action.payload;
+      break;
     case c.DEPOSIT:
-      return state + action.payload;
+      balance = state + action.payload;
+      break;
     case c.WITHDRAW:
-      return state - action.payload;
+      balance = state - action.payload;
+      break;
     default:
-      return state;
+      balance = parseInt(read_cookie(BALANCE_COOKIE), 10) || state;
   }
+
+  bake_cookie(BALANCE_COOKIE, balance);
+
+  return balance;
 };
 
 export default balanceReducer;
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,12 +1,22 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import c from '../actions/constants';
 
 describe('balanceReducer', () => {
-  it('sets the balance', () => {
+  describe('when initializing', () => {
     const balance = 10;
-    const action = { type: c.SET_BALANCE, payload: balance };
 
-    expect(balanceReducer(undefined, action)).toEqual(balance);
+    it('sets the balance', () => {
+      const action = { type: c.SET_BALANCE, payload: balance };
+
+      expect(balanceReducer(undefined, action)).toEqual(balance);
+    });
+
+    describe('then re-initializing', () => {
+      it('reads the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
 
   it('deposits into the balance', () => {
